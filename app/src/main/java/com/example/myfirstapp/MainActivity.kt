9bcfe2c22edc63aa747682256b417c5f5632fa51
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-04-13 21:24:22 +0300

    endof simple counter app

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myBtn = findViewById<Button>(R.id.myBtn)
+        var counter = 0
+        val myText = findViewById<TextView>(R.id.textView)
+        myText.text = counter.toString()
+        myBtn.setOnClickListener {
+            counter++;
+            myText.text = counter.toString()
+            Toast.makeText(this,"You increased the counter", Toast.LENGTH_LONG).show()
+        }
     }
 }
